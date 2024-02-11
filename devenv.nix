6ae04dea273cd053f22e43bbc590ddca4a501329
Sergii Maksymov <ua.maximoff@gmail.com>
2024-02-11 19:12:16 +0100

    initial commit

---
+++ devenv.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+
+{
+  packages = [
+    pkgs.platformio
+    pkgs.openocd
+  ];
+
+  enterShell = ''
+    pio init
+  '';
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
